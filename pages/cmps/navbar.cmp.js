49e62a30641cdc6b296bda81d1ffc76bf1086ecd
Your NameTamir <tamirn7@gmail.com>
2020-03-02 23:21:18 +0200

    Tamir's final commit #2

--- pages/cmps/navbar.cmp.js
+++ pages/cmps/navbar.cmp.js
@@ -1,7 +1,9 @@
 export default {
     template: `
         <div class="navbar">
+        <router-link to="/" exact>
         <img src="./img/logo_transparent.png" alt="" class="appsus-logo">
+       </router-link>
         <div>
             <router-link to="/miss-books" exact>
                <span class="navbar-btn"> Miss Books </span>
